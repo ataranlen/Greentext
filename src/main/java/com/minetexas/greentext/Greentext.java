110a69f15371a181240bada7ad99d193d33b663d
Nathan Stoltenberg <ataranlen@gmail.com>
2022-09-06 11:38:09 -0500

    Fix Placeholders

--- src/main/java/com/minetexas/greentext/Greentext.java
+++ src/main/java/com/minetexas/greentext/Greentext.java
@@ -1,5 +1,7 @@
 package com.minetexas.greentext;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -7,17 +9,21 @@ import com.minetexas.greentext.commands.CommandBase;
 import com.minetexas.greentext.util.GTLog;
 import com.minetexas.greentext.util.GTSettings;
 
-public class Greentext extends JavaPlugin {
+public class Greentext extends JavaPlugin implements Listener {
 	@Override
 	public void onEnable() {
-		
-
+	
 		GTLog.init(this);
 		GTLog.info("onEnable has been invoked!");
 		GTSettings.init(this);
 
-
 		this.getCommand("me").setExecutor(new CommandBase());
+	    this.getCommand("me2").setExecutor(new CommandBase());
+	    if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            Bukkit.getPluginManager().registerEvents(this, this);
+	    } else {
+	    	throw new RuntimeException("Could not find PlaceholderAPI!! Plugin can not work without it!");
+	    } 
 	}
  
 	@Override
--- src/main/java/com/minetexas/greentext/commands/CommandBase.java
+++ src/main/java/com/minetexas/greentext/commands/CommandBase.java
@@ -8,7 +8,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
+import me.clip.placeholderapi.PlaceholderAPI;
 import com.minetexas.greentext.exception.GTException;
 import com.minetexas.greentext.util.GTColor;
 import com.minetexas.greentext.util.GTLog;
@@ -51,10 +51,21 @@ public class CommandBase implements CommandExecutor {
 			return false;
 		} else {
 			StringBuilder builder = new StringBuilder();
-			if (!permissionCheck(GTSettings.PERMISSION_ANON)){
-				builder.append(sender.getName()+" ");
+			String name = sender.getName();
+			if(sender instanceof Player){
+				Player player1 = (Player)sender;
+				name = player1.getName();
 			}
-			builder.append(">");
+			if (!permissionCheck(GTSettings.PERMISSION_ANON)){
+				builder.append(name + " ");
+			    builder.append(">");
+			} else if (label.contains("me2")) {
+			    builder.append("> ");
+			    builder.append(GTColor.LightBlue + sender.getName() + " " + GTColor.LightGreen);
+
+			} else {
+			    builder.append(">");
+			}
 			
 			for(String s : args) {
 				s = s.replace("@p", GTColor.LightBlue+sender.getName()+GTColor.LightGreen);
@@ -67,11 +78,19 @@ public class CommandBase implements CommandExecutor {
 			    builder.append(" "+s);
 			}
 			String meme = builder.toString();
+			if (sender instanceof Player) {
+				Player player1 = (Player)sender;
+				if (meme.contains("%plan_player_") || meme.contains("%plan_sessions_new_players_day") || player1.isOp())
+					meme = PlaceholderAPI.setPlaceholders(player1, meme); 
+				GTLog.info(String.valueOf(sender.getName()) + meme);
+				if (meme.length() > 256) {
+					sendMessage(sender, "the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little \"clever\" comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo.");
+					return false;
+				} 
+		    } 
 			GTLog.info(sender.getName()+meme);
 
-			Collection <? extends Player> players = Bukkit.getOnlinePlayers();//.forEach(p -> p.sendMessage(GTColor.LightGreen+meme));
-
-
+			Collection <? extends Player> players = Bukkit.getOnlinePlayers();
 			Boolean useHerochat = false;
 			if (GTSettings.hasHerochat == true) {
 				Herochat hc = Herochat.getPlugin();
